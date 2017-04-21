9f8690e316d66ce29afb06a7fc17e58de411d77b
Stephen McElhinney <stephen@appstruct.io>
2017-04-21 15:05:54 +0200

    Added CocoaPods

---
+++ ios/Pods/Target Support Files/Pods-InboundApp-tvOS/Pods-InboundApp-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_InboundApp_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_InboundApp_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-InboundApp-tvOSTests/Pods-InboundApp-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_InboundApp_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_InboundApp_tvOSTests
+@end
